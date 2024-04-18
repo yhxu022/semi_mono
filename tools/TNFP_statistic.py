67660f67697e8bf39a27a5dc0a7ceb19846ac126
yhxu022 <840106470@qq.com>
2024-04-18 20:37:58 +0800

    1

--- tools/TNFP_statistic.py
+++ tools/TNFP_statistic.py
@@ -60,16 +60,16 @@ def main():
     cfg = yaml.load(open(args.config, 'r'), Loader=yaml.Loader)
     config_name, _ = os.path.splitext(os.path.basename(args.config))
     save_dir = "statistics"
-	save_TNFP_dir = 'TNFP'
+    save_TNFP_dir = 'TNFP'
     os.makedirs(save_dir, exist_ok=True)
-	os.makedirs(save_TNFP_dir, exist_ok=True)
+    os.makedirs(save_TNFP_dir, exist_ok=True)
     if cfg.get("visualize",False):
         # 检查文件夹是否已经存在
         if not os.path.exists('outputs_visual'):
         # 创建新的文件夹
             os.mkdir('outputs_visual')
     checkpoint = cfg["trainer"].get("pretrain_model", None)
-
+    IOU_thr_glip =  cfg["semi_train_cfg"].get("IOU_thr", 0.7)
     print("start statistics:")
     print(f"loading from CONFIG {checkpoint}")
     unlabeled_dataset = KITTI_Dataset(split=cfg["dataset"]["inference_split"], cfg=cfg['dataset'])
@@ -112,7 +112,7 @@ def main():
         FP_pic = 0
         FN_pic = 0
         wrong_labels = []
-		input_teacher = inputs[1]
+        input_teacher = inputs[1]
         input_teacher = input_teacher.to("cuda")
         calib = calib.to("cuda")
         id = int(info['img_id'])
@@ -267,9 +267,9 @@ def main():
             if max_iou_indices_2d[idx] not in idx_selected_2d:
                 if pred_label_2d == gt_label_2d:
                     all_TP_2d = all_TP_2d + 1
-					TP_pic = TP_pic + 1
+                    TP_pic = TP_pic + 1
                     idx_selected_2d.append(max_iou_indices_2d[idx])
-				else:
+                else:
                     wrong_labels.append(pred_label_2d)
 
 
@@ -299,11 +299,11 @@ def main():
         all_pred_depth_and_cls_scores.extend(pred_depth_and_cls_scores)
         description = f"image idx: {id} | all_gts: {all_gts} | all_preds: {all_preds} | all_TP: {all_TP} | all_TP_2d: {all_TP_2d}"
         progress_bar.set_description(description)
-		PRED_pic = num_pre
+        PRED_pic = num_pre
         GT_pic = num_gt
         FP_pic = num_pre - TP_pic
         FN_pic = num_gt - TP_pic
-		filename = f"{save_TNFP_dir}/TNFP_in_one_picture_IOU_{IOU_thr_glip}.txt"
+        filename = f"{save_TNFP_dir}/TNFP_in_one_picture_IOU_{IOU_thr_glip}.txt"
         with open(filename, "a") as file:
             file.write(f"Image Index---{id}    GT---{GT_pic}    PRED---{PRED_pic}    TP: {TP_pic}    FP: {FP_pic}    FN: {FN_pic}    wronglist:{wrong_labels}\n")
 
@@ -327,7 +327,8 @@ def main():
             cv2.imwrite(f'outputs_visual/KITTI_{cfg["dataset"]["inference_split"]}_{id}_3d.png', img_bbox3d)
             if (cfg["dataset"]["inference_split"] not in ['eigen_clean', 'raw_mix']):
                 cv2.imwrite(f'outputs_visual/KITTI_{cfg["dataset"]["inference_split"]}_{id}_bev.png', img_bev)
-		
+
+
     print(f"all_gts  --  {all_gts}")
     print(f"all_preds  --  {all_preds}")
     print(f"all_TP  --  {all_TP} -- all_TP_2d  --   {all_TP_2d}")
